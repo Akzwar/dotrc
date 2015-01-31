11d25d36536d63c07f1b1404b7c706f1cfad9454
Akzhigitov Anton <akzhigitov.anton@yandex.ru>
2015-01-31 11:28:44 +0300

    some fatal errors

--- i3/init.sh
+++ i3/init.sh
@@ -3,27 +3,12 @@
 synclient TapButton1="1"
 synclient TapButton2="2"
 synclient TapButton3="3"
-synclient TouchpadOff="1"
-
-hdmi=`xrandr | grep HDMI1 | grep disconnected`
 
 xset s off && xset -dpms
 
-if test x"$hdmi" = x ; then
-    xrandr --output LVDS1 --mode 1920x1080 --pos 1920x300 --output HDMI1 --rotate normal --mode 1920x1200 --pos 0x0
-fi
-
-feh --bg-scale /home/deviator/Pictures/bg.png
-
-xbacklight -set 60%
+xbacklight -set 100%
 
 numlockx on
-
-dropbox start
-
-#bluetooth-applet &
-nm-applet &
-#gnome-sound-applet &
 
 # /usr/share/X11/xkb/symbols/group
 setxkbmap -model evdev -layout 'us,ru' -option -option 'grp:lctrl_rctrl_switch,grp:lswitch'
